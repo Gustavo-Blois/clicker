246d7bebba152d1198c1379a9687302ee58a475b
Gustavo Blois <gustavoblois@gmail.com>
2025-11-29 15:07:02 -0300

    Gerenciador de threads

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #include <limits.h>
 #include <stdio.h>
 #include <time.h>
-
+#include <semaphore.h>
+#include <pthread.h>
 
 // STOVE: STOVE
 // CTBD: cutting board
@@ -27,50 +28,85 @@ typedef struct player_st{
     Pos pos;
 } Player;
 
-#define LEVEL_SIZE 10
+typedef struct customer_st{
+    Pos pos;
+} Customer;
+
+#define LEVEL_SIZE_Y 11
+#define LEVEL_SIZE_X 10
+#define N_THREADS LEVEL_SIZE_X
 #define screenHeight 500
 #define screenWidth 500
 
 #include "boards.h"
+const char *game_over_message = "O JOGO ACABOU";
+int GAME_OVER = 0;
 
-void render(block board[LEVEL_SIZE][LEVEL_SIZE], Player *p1){
+void render(block board[LEVEL_SIZE_Y][LEVEL_SIZE_X], Player *p1, int customers[N_THREADS]){
 
-    Vector2 offset = {screenWidth%LEVEL_SIZE,screenHeight%LEVEL_SIZE};
+   // Vector2 offset = {screenWidth%LEVEL_SIZE_X,screenHeight%LEVEL_SIZE_Y};
     BeginDrawing();
     ClearBackground(GRAY);
 
-    
+    if (GAME_OVER == 1) {
+        ClearBackground(RED);
+        DrawText(game_over_message, screenWidth/2, screenHeight/2, 20, WHITE);
+    } else{
     Vector2 sizeofsquare = {
-        screenWidth/LEVEL_SIZE ,
-        screenHeight/LEVEL_SIZE ,
+        screenWidth/LEVEL_SIZE_X ,
+        screenHeight/LEVEL_SIZE_Y ,
     };
-
-    for(int i = 0; i < LEVEL_SIZE; i++){
-        for(int j = 0; j < LEVEL_SIZE; j++){
-			switch(board[i][j]) {
-				case FLOR:
-					DrawRectangleV((Vector2){sizeofsquare.x*j,sizeofsquare.y*i},sizeofsquare,(Color){50, 50, 50, 255});
-					break;
-				case WALL:
-					DrawRectangleV((Vector2){sizeofsquare.x*j,sizeofsquare.y*i},sizeofsquare,(Color){150, 150, 150, 255});
-					break;
-				case CTBD:
-					DrawRectangleV((Vector2){sizeofsquare.x*j,sizeofsquare.y*i},sizeofsquare,(Color){255, 255, 255, 255});
-					break;
-				case STOVE:
-					DrawRectangleV((Vector2){sizeofsquare.x*j,sizeofsquare.y*i},sizeofsquare,(Color){50, 50, 255, 255});
-					break;
-				case DLVR:
-					DrawRectangleV((Vector2){sizeofsquare.x*j,sizeofsquare.y*i},sizeofsquare,(Color){50, 255, 50, 255});
-					break;
-			}
+for (int y = 0; y < LEVEL_SIZE_Y; y++) {
+    for (int x = 0; x < LEVEL_SIZE_X; x++) {
+        switch (board[y][x]) {
+            case FLOR:
+                DrawRectangleV(
+                    (Vector2){ sizeofsquare.x * x, sizeofsquare.y * y },
+                    sizeofsquare,
+                    (Color){50, 50, 50, 255}
+                );
+                break;
+            case WALL:
+                DrawRectangleV(
+                    (Vector2){ sizeofsquare.x * x, sizeofsquare.y * y },
+                    sizeofsquare,
+                    (Color){150, 150, 150, 255}
+                );
+                break;
+            case CTBD:
+                DrawRectangleV(
+                    (Vector2){ sizeofsquare.x * x, sizeofsquare.y * y },
+                    sizeofsquare,
+                    (Color){255, 255, 255, 255}
+                );
+                break;
+            case STOVE:
+                DrawRectangleV(
+                    (Vector2){ sizeofsquare.x * x, sizeofsquare.y * y },
+                    sizeofsquare,
+                    (Color){50, 50, 255, 255}
+                );
+                break;
+            case DLVR:
+                DrawRectangleV(
+                    (Vector2){ sizeofsquare.x * x, sizeofsquare.y * y },
+                    sizeofsquare,
+                    (Color){50, 255, 50, 255}
+                );
+                break;
         }
     }
-    for (int i = 0; i < screenWidth/LEVEL_SIZE + 1; i++){
+}
+    for(int i = 0; i < N_THREADS; i++) {
+        if (customers[i] != 0){
+            DrawRectangleV((Vector2){sizeofsquare.x*i,0},sizeofsquare,BLUE);
+        }
+    }
+    for (int i = 0; i < screenWidth/LEVEL_SIZE_X + 1; i++){
         DrawLineV((Vector2){i*sizeofsquare.x, 0}, (Vector2){i*sizeofsquare.x, screenHeight}, LIGHTGRAY);
     }
 
-    for (int i = 0; i < screenHeight/LEVEL_SIZE + 1; i++){
+    for (int i = 0; i < screenHeight/LEVEL_SIZE_Y + 1; i++){
         DrawLineV((Vector2){0, i*sizeofsquare.y}, (Vector2){screenWidth, i*sizeofsquare.y}, LIGHTGRAY);
     }
 
@@ -78,16 +114,17 @@ void render(block board[LEVEL_SIZE][LEVEL_SIZE], Player *p1){
 
 
 
+}
     EndDrawing();
 
 }
 
-int position_player(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
-    for(int i = 0; i < LEVEL_SIZE; i++){
-        for(int j = 0; j < LEVEL_SIZE; j++){
+int position_player(block board[LEVEL_SIZE_Y][LEVEL_SIZE_X],Player *p1){
+    for(int i = 1; i < LEVEL_SIZE_Y; i++){
+        for(int j = 0; j < LEVEL_SIZE_X; j++){
             if (board[i][j] == FLOR) {
-                p1->pos.x = i;
-                p1-> pos.y = j;
+                p1->pos.x = j;
+                p1-> pos.y =i;
                 return 0;
             }
         }
@@ -95,49 +132,85 @@ int position_player(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
     return -1;
 }
 
-void moveUp(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
-    if (p1->pos.y < LEVEL_SIZE){
+void moveUp(block board[LEVEL_SIZE_Y][LEVEL_SIZE_X],Player *p1){
+    if (p1->pos.y < LEVEL_SIZE_Y){
         if (board[p1->pos.y - 1][p1->pos.x] == FLOR){
             p1->pos.y -= 1;
         }
     }
 }
 
-void moveDown(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
-    if (p1->pos.y < LEVEL_SIZE){
+void moveDown(block board[LEVEL_SIZE_Y][LEVEL_SIZE_X],Player *p1){
+    if (p1->pos.y > 0){
         if (board[p1->pos.y + 1][p1->pos.x] == FLOR){
             p1->pos.y += 1;
         }
     }
 }
 
-void moveLeft(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
-    if (p1->pos.x < LEVEL_SIZE){
+void moveLeft(block board[LEVEL_SIZE_Y][LEVEL_SIZE_X],Player *p1){
+    if (p1->pos.x > 0){
         if (board[p1->pos.y][p1->pos.x - 1] == FLOR){
             p1->pos.x -= 1;
         }
     }
 }
 
-void moveRight(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
-    if (p1->pos.x < LEVEL_SIZE){
+void moveRight(block board[LEVEL_SIZE_Y][LEVEL_SIZE_X],Player *p1){
+    if (p1->pos.x < LEVEL_SIZE_X){
         if (board[p1->pos.y][p1->pos.x + 1] == FLOR){
             p1->pos.x += 1;
         }
     }
 }
 
+int find_open_thread_index(int threads[N_THREADS]){
+    for(int i = 0; i < N_THREADS; i++){
+        if (threads[i] == NULL){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void * imprime_thread(void* numero_thread){
+    printf("Alocada a thread %d\n",(int*) numero_thread);
+}
+
+
+
 int main(){
     srand(time(NULL));
     InitWindow(500,500,"clicker");
     SetTargetFPS(60);
     Player p1;
+    int customer_buffer[N_THREADS] = {0};
+    pthread_t customer_threads[N_THREADS];
     if(position_player(map2, &p1) < 0){
         printf("Could not position player\n");
         return -1;
     }
+
+    time_t initial_time = time(NULL); 
+
     while(!WindowShouldClose()){
-        render(map2,&p1);
+        render(map2,&p1,customer_buffer);
+        time_t current_time = time(NULL);
+        if (difftime(current_time, initial_time) >= 2.0){
+            initial_time = current_time;
+            int customer_thread_id = find_open_thread_index(customer_buffer);
+            if (customer_thread_id < 0){
+                GAME_OVER = 1;
+            } else {
+                if (pthread_create(&customer_threads[customer_thread_id],NULL, imprime_thread,(void*)customer_thread_id) != 0){
+                    printf("Erro ao criar thread\n");
+                    return 1;
+                }
+                customer_buffer[customer_thread_id] = 1;
+
+            }
+            printf("Se passaram 10 segundos\n");
+        }
         if (IsKeyPressed('Q')){
             CloseWindow();
             break;
