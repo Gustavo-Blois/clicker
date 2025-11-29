948e98f5f4b82bcb58c1950f1f737f23159ddebb
Gustavo Blois <gustavoblois@gmail.com>
2025-11-29 13:34:13 -0300

    Lógica de movimentação do jogador criada

--- main.c
+++ main.c
@@ -5,18 +5,27 @@
 #include <stdio.h>
 #include <time.h>
 
-// CKTP: cooktop
+
+// STOVE: STOVE
 // CTBD: cutting board
 // FLOR: floor
 // DLVR: deliver (onde entregamos os pedidos)
 typedef enum block{
 WALL,
-CKTP,
+STOVE,
 CTBD,
 FLOR,
 DLVR,
 } block;
 
+typedef struct pos_st {
+    int x;
+    int y;
+} Pos;
+
+typedef struct player_st{
+    Pos pos;
+} Player;
 
 #define LEVEL_SIZE 10
 #define screenHeight 500
@@ -24,7 +33,7 @@ DLVR,
 
 #include "boards.h"
 
-void render(block board[LEVEL_SIZE][LEVEL_SIZE]){
+void render(block board[LEVEL_SIZE][LEVEL_SIZE], Player *p1){
 
     Vector2 offset = {screenWidth%LEVEL_SIZE,screenHeight%LEVEL_SIZE};
     BeginDrawing();
@@ -48,7 +57,7 @@ void render(block board[LEVEL_SIZE][LEVEL_SIZE]){
 				case CTBD:
 					DrawRectangleV((Vector2){sizeofsquare.x*j,sizeofsquare.y*i},sizeofsquare,(Color){255, 255, 255, 255});
 					break;
-				case CKTP:
+				case STOVE:
 					DrawRectangleV((Vector2){sizeofsquare.x*j,sizeofsquare.y*i},sizeofsquare,(Color){50, 50, 255, 255});
 					break;
 				case DLVR:
@@ -65,6 +74,7 @@ void render(block board[LEVEL_SIZE][LEVEL_SIZE]){
         DrawLineV((Vector2){0, i*sizeofsquare.y}, (Vector2){screenWidth, i*sizeofsquare.y}, LIGHTGRAY);
     }
 
+    DrawRectangleV((Vector2){sizeofsquare.x*p1->pos.x,sizeofsquare.y*p1->pos.y},sizeofsquare,RED);
 
 
 
@@ -72,20 +82,77 @@ void render(block board[LEVEL_SIZE][LEVEL_SIZE]){
 
 }
 
+int position_player(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
+    for(int i = 0; i < LEVEL_SIZE; i++){
+        for(int j = 0; j < LEVEL_SIZE; j++){
+            if (board[i][j] == FLOR) {
+                p1->pos.x = i;
+                p1-> pos.y = j;
+                return 0;
+            }
+        }
+    }
+    return -1;
+}
+
+void moveUp(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
+    if (p1->pos.y < LEVEL_SIZE){
+        if (board[p1->pos.y - 1][p1->pos.x] == FLOR){
+            p1->pos.y -= 1;
+        }
+    }
+}
+
+void moveDown(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
+    if (p1->pos.y < LEVEL_SIZE){
+        if (board[p1->pos.y + 1][p1->pos.x] == FLOR){
+            p1->pos.y += 1;
+        }
+    }
+}
+
+void moveLeft(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
+    if (p1->pos.x < LEVEL_SIZE){
+        if (board[p1->pos.y][p1->pos.x - 1] == FLOR){
+            p1->pos.x -= 1;
+        }
+    }
+}
+
+void moveRight(block board[LEVEL_SIZE][LEVEL_SIZE],Player *p1){
+    if (p1->pos.x < LEVEL_SIZE){
+        if (board[p1->pos.y][p1->pos.x + 1] == FLOR){
+            p1->pos.x += 1;
+        }
+    }
+}
+
 int main(){
-    int clicks = 0;
     srand(time(NULL));
     InitWindow(500,500,"clicker");
     SetTargetFPS(60);
+    Player p1;
+    if(position_player(map2, &p1) < 0){
+        printf("Could not position player\n");
+        return -1;
+    }
     while(!WindowShouldClose()){
-        render(map2);
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-            clicks += 1;
-            printf("clicks: %d\n",clicks);
-        }
+        render(map2,&p1);
         if (IsKeyPressed('Q')){
             CloseWindow();
             break;
+        }
+        if (IsKeyPressed('W')){
+            moveUp(map2,&p1);
+        }
+        if (IsKeyPressed('S')){
+            moveDown(map2,&p1);
+        }
+        if (IsKeyPressed('A')){
+            moveLeft(map2,&p1);
+        }
+        if (IsKeyPressed('D')){
+            moveRight(map2,&p1);
         }
     }
 
